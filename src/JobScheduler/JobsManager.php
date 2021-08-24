f81683fa596c6dcb00d8cc7b6d21daff55fd9ad4
rodrigomarques <rodrigomarques@pbh.gov.br>
2021-08-24 19:54:36 -0300

    criado o esqueleto da classe de gerenciamento de jobs

---
+++ src/JobScheduler/JobsManager.php
@@ -0,0 +1,30 @@
+<?php
+
+use JobScheduler\Job;
+
+namespace JobScheduler;
+
+class JobsManager {
+
+    private $jobs = [];
+
+    public function adicionarJob(Job $job)
+    {
+
+    }
+    
+    public function retornarJobsPorJanelaExecucao($dataHoraInicio, $dataHoraFim)
+    {
+        
+    }
+
+    public function executarJob(Job $job)
+    {
+
+    }
+
+    public function executarFilaDeJobs($dataHoraInicio, $dataHoraFim)
+    {
+
+    }
+}
