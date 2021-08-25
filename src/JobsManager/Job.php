986e28383ac128dd14b23ec99c25dd7bdc1fc181
rodrigomarques <rodrigomarques@pbh.gov.br>
2021-08-25 18:05:03 -0300

    adicionado validacao para a descricao

--- src/JobsManager/Job.php
+++ src/JobsManager/Job.php
@@ -10,6 +10,23 @@ class Job {
     private $tempoEstimado;
 
     public function __construct($id, $descricao, $dataMaximaConclusao, $tempoEstimado) {
+        
+        if (!is_int($id)) {
+            throw new \InvalidArgumentException('O id deve ser inteiro');
+        }
+        
+        if (empty($descricao)) {
+            throw new \InvalidArgumentException('A descricao nao pode ser vazia');
+        }
+
+        if (!strtotime($dataMaximaConclusao)) {
+            throw new \InvalidArgumentException('A dataMaximaConclusao deve obedecer o formato yyyy-mm-dd hh:mm:ss');
+        }
+
+        if (!is_int($tempoEstimado)) {
+            throw new \InvalidArgumentException('O tempoEstimado deve ser inteiro');
+        }
+
         $this->id = $id;
         $this->descricao = $descricao;
         $this->dataMaximaConclusao = $dataMaximaConclusao;
