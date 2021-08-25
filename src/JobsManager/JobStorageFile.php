6aa099ceb83c884e63ba337f4b91bb9bd0ae29f6
rodrigomarques <rodrigomarques@pbh.gov.br>
2021-08-25 18:06:43 -0300

    criacao da classe de storage de armazenamento em arquivo

---
+++ src/JobsManager/JobStorageFile.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace JobsManager;
+
+use JobsManager\Job;
+use JobsManager\iStorage;
+
+class JobStorageFile implements iStorage {
+
+    private $filePath;
+
+    public function __construct($filePath)
+    {
+        if (!is_writable(dirname($filePath))) {
+            throw new \InvalidArgumentException("O local de armazenamento de jobs {$filePath} não possui permissões de escrita.");
+        }
+        
+        $this->filePath = $filePath;
+    }
+
+    public function salvarJob(Job $job)
+    {
+        $jobsCadastrados = $this->retornarJobs();
+
+        $listaJobs = [];
+        foreach($jobsCadastrados as $jobCorrente) {
+            $listaJobs[] = $jobCorrente->toArray();
+        }
+
+        $listaJobs[] = $job->toArray();
+        
+        file_put_contents($this->filePath, json_encode($listaJobs));
+        return $job;
+    }
+
+    public function retornarJobs()
+    {
+        $listaJobs = json_decode(trim(file_get_contents($this->filePath)));
+
+        $jobs = [];
+        foreach ($listaJobs as $job) {
+            $objJob = new Job($job->id, $job->descricao, $job->dataMaximaConclusao, $job->tempoEstimado);
+            $jobs[] = $objJob;
+        }
+
+        return $jobs;
+    }
+
+    public function excluirJobs()
+    {
+        unlink($this->filePath);
+    }
+}
