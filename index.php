242cd88bef5ce76184203da3f17f79d23731e8fa
rodrigomarques <rodrigomarques@pbh.gov.br>
2021-08-25 21:40:42 -0300

    criacao de pagina de apresentacao

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
 use JobsManager\Job;
 use JobsManager\JobsManager;
 use JobsManager\JobStorageFile;
+use \Psr\Http\Message\ResponseInterface as Response;
+use \Psr\Http\Message\ServerRequestInterface as Request;
 
-require_once  __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 date_default_timezone_set('America/Sao_Paulo');
 
@@ -18,6 +18,44 @@ $configuration = [
 $container = new \Slim\Container($configuration);
 
 $app = new \Slim\App($container);
+
+$app->get('/', function (Request $request, Response $response) {
+
+    $resultTests = shell_exec('./vendor/bin/phpunit tests --testdox');
+
+    $template = <<<HTML
+
+<!DOCTYPE html>
+<html lang="pt">
+  <head>
+    <meta charset="utf-8">
+    <title>Scheduling Job</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
+</head>
+  <body>
+    <section class="py-5 jumbotron text-center">
+        <div class="container">
+            <h1 class="jumbotron-heading">Scheduling Job</h1>
+            <h6>Desafio: Criar algoritmo que retorne um conjunto de arrays com as seguintes características:</h6>
+            <ul class="list-unstyled">
+                <li>Cada array do conjunto representa uma lista de Jobs a serem executados em sequência;</li>
+                <li>Cada array deve conter jobs que sejam executados em, no máximo, 8h;</li>
+                <li>Deve ser respeitada a data máxima de conclusão do Job;</li>
+                <li>Todos os Jobs devem ser executados dentro da janela de execução (data início e fim).</li>
+            </ul>
+            <h6>Resultado dos testes unitários</h6>
+            <code><pre>{$resultTests}</pre></code>
+        </div>
+        
+        <a href="#" class="btn btn-primary my-2">Acessar GitHub</a>
+    </section>
+    
+  </body>
+</html>
+HTML;
+
+    echo $template;
+});
 
 $app->get('/listar-fila-jobs/{dataHoraInicio}/{dataHoraFim}', function (Request $request, Response $response, array $args) {
 
@@ -47,13 +85,12 @@ $app->post('/adicionar-job', function (Request $request, Response $response) {
     }
 });
 
-
 $app->post('/excluir-jobs', function (Request $request, Response $response) {
-    
+
     try {
         $jobStorageFile = new JobStorageFile(__DIR__ . '/job.txt');
         $jobStorageFile->excluirJobs();
-        
+
         $response->withJson(['message' => 'Jobs exluidos com sucesso!'], 200);
 
     } catch (\Exception $e) {
